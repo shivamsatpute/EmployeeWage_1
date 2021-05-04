4619834eaed88eadda28888f51ff4c424100b807
shivam satpute <shivam897satpute@gmail.com>
2021-05-04 22:35:28 +0530

    [shivam]Add: Employee Wage For Month

--- EmployeeAttendance/Program.cs
+++ EmployeeAttendance/Program.cs
@@ -9,27 +9,36 @@ namespace PracticeProblem
         public const int IS_FULL_TIME = 1;     //Constant variable
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+
         public static void Main(string[] args)      //Main method
         {
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
             Random random = new Random();       //Random Class
-            int empCheck = random.Next(0, 3);      //Random Generate 0 ,1,2
-            switch (empCheck)          //Switch case Statment
+            for (int Day = 0; Day < NUM_OF_WORKING_DAYS; Day++)
             {
-                case IS_FULL_TIME:         //Employee is FullTime=1
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:          //Employee is FullTime=2
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
+                int empCheck = random.Next(0, 3);      //Random Generate 0 ,1,2
+                switch (empCheck)          //Switch case Statment
+                {
+                    case IS_FULL_TIME:         //Employee is FullTime=1
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:          //Employee is FullTime=2
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
 
-            empWage = EMP_RATE_PER_HOUR * empHrs;       // Calculate empWage
-            Console.WriteLine("Employe Wage Per Day:- " + empWage);     //Display empwage
+                empWage = EMP_RATE_PER_HOUR * empHrs; // Calculate empWage
+                totalEmpWage = totalEmpWage + empWage;
+            }
+                Console.WriteLine("Employe Wage Per Day:- " + empWage);     //Display empwage
+            Console.WriteLine("Total Employe Month Wage :- " + totalEmpWage);
+
         }
     }
 }
