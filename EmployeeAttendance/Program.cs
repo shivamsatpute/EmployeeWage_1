e42d4827ad83c4aa73f13652734509049788c35b
shivam satpute <shivam897satpute@gmail.com>
2021-05-04 21:12:43 +0530

    [shivam]Add: Checking Employee Present Or Absent

---
+++ EmployeeAttendance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeAttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+            if (EmpCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee Is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employe Is Absent");
+            }
+
+        }
+    }
+}
